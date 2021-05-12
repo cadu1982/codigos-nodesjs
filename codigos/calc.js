b0ecca7f37be2d9dbfafa24f04952feba1fafa9e
Carlos Ponce <cadu.lp@gmail.com>
2021-05-12 19:41:59 -0300

    Olá Diego

---
+++ codigos/calc.js
@@ -0,0 +1,9 @@
+var SomaFunc = require("./soma");
+var SubFunc = require("./sub");
+var DivFunc = require("./div");
+var MultFunc = require("./mult");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(30,20));
+console.log(DivFunc(100,50));
+console.log(MultFunc(10,13));
---
+++ codigos/calculadora.js
@@ -0,0 +1,18 @@
+function div(a,b){
+return a/b;
+}
+
+function mult(a,b){
+return a*b;
+}
+
+function soma(a,b){
+return a+b;
+}
+
+function sub(a,b){
+return a-b;
+}
+
+
+console.log(sub(2,4));
